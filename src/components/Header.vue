d0c8ca32b2c1c05bd66e71c4794bf1de1ad3aa5f
Muhammed ogunsanya <mspeaks910@gmail.com>
2020-02-03 11:45:53 +0100

    Bootstrap Vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,8 @@
 <template>
-<div><h1>Header</h1></div>
+<div>
+    <b-nav tabs>
+        <b-nav-item disabled> <b>Q App</b></b-nav-item>
+        <b-nav-item disabled>Counter: 4/10</b-nav-item>
+    </b-nav>
+</div>
 </template>
-
-<script>
-export default {
-    
-}
-</script>
--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,11 +1,19 @@
 <template>
-<div>
-    <h1>Question Box</h1>
-</div>
+  <div class="question-box-container">
+    <b-jumbotron>
+      <template v-slot:lead>
+        Questions
+      </template>
+
+      <hr class="my-4" />
+
+      <p>
+        Answers
+      </p>
+
+      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button variant="success" href="#">Next</b-button>
+    </b-jumbotron>
+  </div>
 </template>
 
-<script>
-export default {
-    
-}
-</script>
